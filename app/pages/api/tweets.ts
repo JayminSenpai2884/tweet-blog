acc1d2945d78e5ebf225dc2a0ff1ed4d58388ba4
JayminSenpai2884 <jayminparmar2884@gmail.com>
2024-10-31 12:51:13 -0400

    Update files

--- app/pages/api/tweets.ts
+++ app/pages/api/tweets.ts
@@ -1,26 +1,24 @@
 // pages/api/tweets.ts
-import axios from 'axios';
-import type { NextApiRequest, NextApiResponse } from 'next';
-
-const TWITTER_BEARER_TOKEN = process.env.TWITTER_BEARER_TOKEN;
+import { NextApiRequest, NextApiResponse } from 'next';
 
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
-  if (req.method === 'GET') {
-    try {
-      const tweetId = '1851940024394088448'; // Replace with the actual tweet ID
-      const response = await axios.get(`https://api.twitter.com/2/tweets/${tweetId}`, {
-        headers: {
-          Authorization: `Bearer ${TWITTER_BEARER_TOKEN}`,
-        },
-      });
+  const url = `https://api.twitter.com/2/users/by/username/YOUR_USERNAME/tweets`;
+  
+  try {
+    const response = await fetch(url, {
+      headers: {
+        Authorization: `Bearer ${process.env.TWITTER_BEARER_TOKEN}`,
+      },
+    });
 
-      // Send back the response data
-      res.status(200).json(response.data.data);
-    } catch (error) {
-      res.status(500).json({ error: (error as Error).message });
+    if (!response.ok) {
+      throw new Error(`HTTP error! status: ${response.status}`);
     }
-  } else {
-    res.setHeader('Allow', ['GET']);
-    res.status(405).end(`Method ${req.method} Not Allowed`);
+
+    const data = await response.json();
+    res.status(200).json(data);
+  } catch (error) {
+    const errorMessage = (error as Error).message;
+    res.status(500).json({ error: errorMessage });
   }
 }
--- app/pages/index.tsx
+++ app/pages/index.tsx
@@ -1,12 +1,26 @@
-// pages/index.js
-import React from 'react';
-import TweetFetcher from '../components/TweetFetcher';
+// pages/index.tsx
+"use client"
+import { useEffect } from 'react';
 
 export default function Home() {
+  useEffect(() => {
+    const script = document.createElement('script');
+    script.src = 'https://platform.twitter.com/widgets.js';
+    script.async = true;
+    document.body.appendChild(script);
+
+    // Cleanup the script when the component unmounts
+    return () => {
+      document.body.removeChild(script);
+    };
+  }, []);
+
   return (
-    <div style={{ padding: '20px' }}>
-      <h1>Latest Tweets</h1>
-      <TweetFetcher />
+    <div>
+      <h1>My Twitter Feed</h1>
+      <blockquote className="twitter-tweet">
+        <a href="https://twitter.com/jaiminp90840206/status/1851940024394088448"></a>
+      </blockquote>
     </div>
   );
 }
